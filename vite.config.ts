00f2fc51f75a6c60e44cdc096c8c4db57a78202c
rajatlal-source <rajat.lal@salaryse.com>
2025-08-25 14:07:26 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '' // set to '/repo-name/' if deploying under username.github.io/repo-name
+  base: '/salaryse-cc-training2/' // pre-set for this repo
 })
